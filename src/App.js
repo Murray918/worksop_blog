62eb955c295eee5c5bed74d2ce589bef85970927
Andrew Murray <darbyguitars@gmail.com>
2019-06-14 10:56:37 -0500

    adding all the things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Main from './main'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	state = {
+		currentUser: 'Muhr',
+		blogEntries: [
+			{
+				user: 'Muhr',
+				entry:
+					'Nostrud qui culpa nostrud labore laboris minim laboris incididunt ex irure laboris anim cillum. Lorem culpa mollit minim deserunt duis voluptate irure et proident eu fugiat. Velit ad excepteur ut deserunt. Consequat aliqua aliquip quis duis est labore amet ad nulla.',
+				date: Date.now()
+			},
+			{
+				user: 'Muhr',
+				entry:
+					'Ut elit sunt ad id voluptate pariatur nulla ad duis in ut. Elit incididunt consectetur amet non nisi id aliquip esse ut. Et proident esse eu magna cillum ea consequat consequat eiusmod.',
+				date: Date.now()
+			},
+			{
+				user: 'Muhr',
+				entry:
+					'Ut elit sunt ad id voluptate pariatur nulla ad duis in ut. Elit incididunt consectetur amet non nisi id aliquip esse ut. Et proident esse eu magna cillum ea consequat consequat eiusmod.',
+				date: Date.now()
+			}
+		]
+	}
+
+	render() {
+		return (
+			<div className="App container">
+				<header className="App-header">
+					<h1>Welcome to our first React app!</h1>
+				</header>
+				<section>
+					<img src={logo} className="App-logo" alt="logo" />
+					<Main blogEntries={this.state.blogEntries} />
+				</section>
+			</div>
+		)
+	}
 }
-
-export default App;
