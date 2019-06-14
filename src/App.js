9680e069ff85c0024296e650cce522284db4bc92
Andrew Murray <darbyguitars@gmail.com>
2019-06-14 14:06:41 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -4,29 +4,9 @@ import './App.css'
 import Main from './main'
 
 export default class App extends Component {
-	state = {
-		currentUser: 'Muhr',
-		blogEntries: [
-			{
-				user: 'Muhr',
-				entry:
-					'Nostrud qui culpa nostrud labore laboris minim laboris incididunt ex irure laboris anim cillum. Lorem culpa mollit minim deserunt duis voluptate irure et proident eu fugiat. Velit ad excepteur ut deserunt. Consequat aliqua aliquip quis duis est labore amet ad nulla.',
-				date: Date.now()
-			},
-			{
-				user: 'Muhr',
-				entry:
-					'Ut elit sunt ad id voluptate pariatur nulla ad duis in ut. Elit incididunt consectetur amet non nisi id aliquip esse ut. Et proident esse eu magna cillum ea consequat consequat eiusmod.',
-				date: Date.now()
-			},
-			{
-				user: 'Muhr',
-				entry:
-					'Ut elit sunt ad id voluptate pariatur nulla ad duis in ut. Elit incididunt consectetur amet non nisi id aliquip esse ut. Et proident esse eu magna cillum ea consequat consequat eiusmod.',
-				date: Date.now()
-			}
-		]
-	}
+  state = {
+    isLoggedIn : false
+  }
 
 	render() {
 		return (
@@ -36,7 +16,7 @@ export default class App extends Component {
 				</header>
 				<section>
 					<img src={logo} className="App-logo" alt="logo" />
-					<Main blogEntries={this.state.blogEntries} />
+					<Main />
 				</section>
 			</div>
 		)
